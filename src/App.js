0b93c1fe8790ba1155b0c171b167de9cfe39a444
MauricioRosasKsquare <Mauricio.Rosas@theksquaregroup.com>
2021-11-18 01:30:10 -0600

    Base Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Navbar from './componets/Navbar';
+
 
 function App() {
+
+  const [posts ,setPosts] = useState([])
+  const [isLoading ,setIsLoading] = useState(true)
+  const [error ,setError] = useState(null)
+  
+  useEffect(() => {
+
+    fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(res => {
+        if( !res.ok ){
+          throw Error('Could not fetch the data')
+        }
+        res.json()
+      })
+      .then(data => {
+        setPosts(data)
+        setIsLoading(false)
+        setError(null)
+      })
+      .catch( err => {
+        setIsLoading(false)
+        setError(err.message)
+      })  
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
     </div>
   );
 }
---
+++ src/componets/Navbar.js
@@ -0,0 +1,19 @@
+
+const Navbar = () => {
+    return (
+      <nav className="navbar">
+        <h1>Blog</h1>
+        <div className="links">
+          <a href="/">Home</a>
+          <a href="/">Posts</a>
+          <a href="/" style={{ 
+            color: 'white', 
+            backgroundColor: '#f1356d',
+            borderRadius: '8px' 
+          }}>Login</a>
+        </div>
+      </nav>
+    );
+  }
+   
+  export default Navbar;
